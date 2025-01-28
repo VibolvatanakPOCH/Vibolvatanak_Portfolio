1efd2d9644377561febdd6f40853bdc7e38ca844
Vibolvatanak POCH <pochvibolvatanak@gmail.com>
2025-01-28 22:40:42 +0700

    Add new song and update album art paths

--- script.js
+++ script.js
@@ -13,31 +13,37 @@ const musicPlayer = {
             title: "Bangarang",
             artist: "Skrillex ft. Sirah",
             url: "assets/media/SKRILLEX - Bangarang feat. Sirah [Official Music Video].mp3",
-            art: "assets/media/bangarang.jpg"
+            art: "assets/media/album-art/bangarang.jpg"
         },
         {
             title: "Scary Monsters And Nice Sprites",
             artist: "Skrillex",
             url: "assets/media/Skrillex - Scary Monsters And Nice Sprites (Official Audio).mp3",
-            art: "assets/media/scary-monsters.jpg"
+            art: "assets/media/album-art/scary-monsters.jpg"
         },
         {
             title: "Badders",
             artist: "Skrillex, PEEKABOO, Flowdan & G-Rex",
             url: "assets/media/Skrillex, PEEKABOO, Flowdan, & G-Rex - Badders (Official Audio).mp3",
-            art: "assets/media/badders.jpg"
+            art: "assets/media/album-art/badders.jpg"
         },
         {
             title: "Big Dawgs",
             artist: "Hanumankind ft. Kalmi",
             url: "assets/media/Hanumankind  Big Dawgs  Ft. Kalmi (Official Music Video)  Def Jam India.mp3",
-            art: "assets/media/default-art.jpg"
+            art: "assets/media/album-art/big-dawgs.jpg"
         },
         {
             title: "lalala",
             artist: "bbno$ & y2k",
             url: "assets/media/bbno$, y2k - lalala (Lyrics)  did I really just forget that melody_.mp3",
-            art: "assets/media/default-art.jpg"
+            art: "assets/media/album-art/lalala.jpg"
+        },
+        {
+            title: "Chess Type Beat",
+            artist: "Dancing Rat",
+            url: "assets/media/𝐂𝐡𝐞𝐬𝐬 𝐓𝐲𝐩𝐞 𝐁𝐞𝐚𝐭 (𝐒𝐥𝐨𝐰𝐞𝐝𝐓𝐢𝐤𝐭𝐨𝐤 𝐕𝐞𝐫𝐬𝐢𝐨𝐧) 𝐱 𝐃𝐚𝐧𝐜𝐢𝐧𝐠 𝐑𝐚𝐭 - 𝐉𝐞𝐬𝐮𝐬 𝐌𝐮𝐬𝐜.mp3",
+            art: "assets/media/album-art/chess-type-beat.jpg"
         }
     ],
     currentSong: 0,
